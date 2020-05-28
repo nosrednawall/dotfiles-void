6437fcfb7cfb6f6506d44cd5de667da2e70b01e7
nosrednawall <nosrednawall@gmail.com>
2020-05-28 17:00:02 -0300

    atualizado rofi

---
+++ arquivos/i3/restart-i3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+i3 restart
+sh ~/.config/polybar/launch.sh
+sh ~/.wallpapers/random-wallpaper.sh
--- arquivos/polybar/launch.sh
+++ arquivos/polybar/launch.sh
@@ -9,5 +9,13 @@ pkill -x polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#        MONITOR=$m polybar ~/.config/polybar/config main &
+#          done
+#        else
+#            polybar ~/.config/polybar/config  main &
+#fi
+
 # Launch bar1 and bar2
 polybar -c ~/.config/polybar/config main &
