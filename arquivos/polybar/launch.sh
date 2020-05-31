472e210375ee27afca605c8925437c29ec3339c2
nosrednawall <nosrednawall@gmail.com>
2020-05-31 16:27:10 -0300

    dual monitor config

--- arquivos/polybar/launch.sh
+++ arquivos/polybar/launch.sh
@@ -12,9 +12,11 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 DISPLAY1="$(xrandr -q | grep 'eDP1\|VGA-1' | cut -d ' ' -f1)"
 [ ! -z "$DISPLAY1" ] && MONITOR="$DISPLAY1" polybar top1 &
+[ ! -z "$DISPLAY1" ] && MONITOR="$DISPLAY1" polybar bottom1 &
 
 DISPLAY2="$(xrandr -q | grep 'HDMI1\|DVI-I-1' | cut -d ' ' -f1)"
 [ ! -z $DISPLAY2 ] && MONITOR=$DISPLAY2 polybar top2 &
+#[ ! -z $DISPLAY2 ] && MONITOR=$DISPLAY2 polybar bottom2 &
 
 # Launch bar1 and bar2
 #polybar -c ~/.config/polybar/config main &
