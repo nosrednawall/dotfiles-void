b956ab2948eaa1d1dc04c8e8af11cacba703ae84
nosrednawall <nosrednawall@gmail.com>
2020-06-11 11:16:26 -0300

    reposicionado o polybar e adicionado atalho para wallpapers

--- arquivos/polybar/launch.sh
+++ arquivos/polybar/launch.sh
@@ -12,7 +12,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 DISPLAY1="$(xrandr -q | grep 'eDP1\|VGA-1' | cut -d ' ' -f1)"
 [ ! -z "$DISPLAY1" ] && MONITOR="$DISPLAY1" polybar top1 &
-#[ ! -z "$DISPLAY1" ] && MONITOR="$DISPLAY1" polybar bottom1 &
+[ ! -z "$DISPLAY1" ] && MONITOR="$DISPLAY1" polybar bottom1 &
 
 DISPLAY2="$(xrandr -q | grep 'HDMI1\|DVI-I-1' | cut -d ' ' -f1)"
 [ ! -z $DISPLAY2 ] && MONITOR=$DISPLAY2 polybar top2 &
