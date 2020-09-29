8e1ab1da58d35ef9092073e2a150944eac231baf
nosrednawall <nosrednawall@gmail.com>
2020-09-29 07:55:12 -0300

    add validation for arguments

--- arquivos/scripts/create_shortcut.sh
+++ arquivos/scripts/create_shortcut.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+# Valida se tem os arqumentos necessários para o script funcionar
+if test $* -z;
+  then
+    cat <<< '
+      usage: up [--level <n>| -n <levels>][--help][--version]
+
+      Report bugs to: 
+      up home page:
+'
+  exit 0
+fi
+
 NAME=$1 
 COMMENT=$5
 GENERICNAME=$2
